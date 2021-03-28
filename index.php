ca8f337e36eacb202cc76438eabc9b7b8b54fedc
WhannChatwimon <78945835+WhannChatwimon@users.noreply.github.com>
2021-03-28 21:17:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<html>
+
+<head>
+    <title>เครื่องแปลงเลขฐาน</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+    <link rel="icon" href="logo.png">
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src=" logo.png" alt="" width="40" height="40" class="d-inline-block align-top">
+                เครื่องแปลงเลขฐาน
+            </a>
+            <a class="navbar-brand" href="team.php">
+                <img src="team.png" alt="" width="40" height="40" class="d-inline-block align-top">
+                ผู้จัดทำ
+            </a>
+        </div>
+    </nav>
+    <div class="container">
+        <form name="form1" method="post" action="index.php">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="h1">เครื่องแปลงเลขฐาน</h1>
+                </div>
+            </div>
+            <div class="row">
+                <p class="h3">ป้อนค่าที่ต้องการแปลง
+                    <input name="num1" type="text" class="form-control" style="width: 200px; 
+                    display: inline" />
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p class="h3">จากเลขฐาน
+                        <select name="base1">
+                            <option value="2" selected>2</option>
+                            <option value="8">8</option>
+                            <option value="10">10</option>
+                            <option value="16">16</option>
+                        </select>
+                        แปลงเป็นเลขฐาน
+                        <select name="base2">
+                            <option value="2" selected>2</option>
+                            <option value="8">8</option>
+                            <option value="10">10</option>
+                            <option value="16">16</option>
+                        </select>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p class="h3">ผลลัพท์คือ :
+                        <?php
+                        $total = base_convert($_POST['num1'], $_POST['base1'], $_POST['base2']);
+                        echo $total;
+                        ?>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <input name="submit" type="submit" value="คำนวณ" class="btn btn-primary"
+                        style="text-align: center;" />
+                </div>
+            </div>
+        </form>
+        <p class="lead">Binary Number System, Octal Number System, Decimal
+            Number System, Hexadecimal Number system.
+        </p>
+    </div>
+
+</body>
+
+</html>
---
+++ team.php
@@ -0,0 +1,64 @@
+<html>
+
+<head>
+    <title>ผู้จัดทำ</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+    <link rel="icon" href="team.png">
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="logo.png" alt="" width="40" height="40" class="d-inline-block align-top">
+                เครื่องแปลงเลขฐาน
+            </a>
+            <a class="navbar-brand" href="team.php">
+                <img src="team.png" alt="" width="40" height="40" class="d-inline-block align-top">
+                ผู้จัดทำ
+            </a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <h1 class="h1">ผู้จัดทำ</h1>
+            <div class="card" style="width: 29rem;">
+                <img src="avatar1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">นางสาวฉัตรวิมล หวังสบาย</h5>
+                    <p class="card-text">รหัสประจำตัว 63309012004</p>
+                </div>
+            </div>
+            <div class="card" style="width: 29em;">
+                <img src="avatar2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">นางสาวชนาธิป เวียนประโคน</h5>
+                    <p class="card-text">รหัสประจำตัว 63309012005</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="card" style="width: 29rem;">
+                <img src="avatar3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">นางสาวน้ำฝน จันตรี</h5>
+                    <p class="card-text">รหัสประจำตัว 63309012007</p>
+                </div>
+            </div>
+            <div class="card" style="width: 29em;">
+                <img src="avatar4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">นางสาวสุพัตรา เดโชรัมย์</h5>
+                    <p class="card-text">รหัสประจำตัว 63309012017</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
